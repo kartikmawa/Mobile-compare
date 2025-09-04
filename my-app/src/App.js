ba5d8b3ec27c19bbdf17ca14697b34bfd68c9781
kartikmawa <kartik.21810747@viit.ac.in>
2025-09-04 21:41:32 +0530

    Setup project structure with frontend and backend

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>luliu</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://meet.google.com/phd-exse-iww"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Phuddi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
